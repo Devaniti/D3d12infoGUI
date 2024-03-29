47b38b9ca783588388df080f240923affff1349d
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2024-03-29 02:10:06 +0200

    Embeded open source licenses

--- source/GUI/private/main.cpp
+++ source/GUI/private/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace D3d12infoGUI;
 
-int mainImpl(HINSTANCE hInstance) {
+int RunGUI(HINSTANCE hInstance) {
   Window window(hInstance, L"Extracting files");
 
   std::filesystem::path rootPath =
@@ -42,8 +42,9 @@ int mainImpl(HINSTANCE hInstance) {
   if (window.IsExitRequested()) return 1;
   window.ReportProgress(L"Running D3d12info_preview");
 
-  const Subprocess::ProcessOutput previewReport = Subprocess::GetCommandOutput(
-      d3d12infoPreviewPath.string() + " -j --AllAdapters --EnableExperimental=ON");
+  const Subprocess::ProcessOutput previewReport =
+      Subprocess::GetCommandOutput(d3d12infoPreviewPath.string() +
+                                   " -j --AllAdapters --EnableExperimental=ON");
   if (previewReport.first == 0) {
     validReports.push_back(previewReport.second);
   }
@@ -61,10 +62,99 @@ int mainImpl(HINSTANCE hInstance) {
   return 0;
 }
 
+void InitializeConsole() {
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS)) {
+    if (!::AllocConsole()) {
+      throw std::runtime_error("Failed to allocate console");
+    }
+  }
+
+  FILE* dummy;
+  ::freopen_s(&dummy, "CONOUT$", "w", stdout);
+  ::freopen_s(&dummy, "CONOUT$", "w", stderr);
+  ::freopen_s(&dummy, "CONIN$", "r", stdin);
+  std::cout.clear();
+  std::cerr.clear();
+  std::cin.clear();
+
+  HANDLE hConOut = CreateFile(L"CONOUT$", GENERIC_READ | GENERIC_WRITE,
+                              FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+                              OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  HANDLE hConIn = CreateFile(L"CONIN$", GENERIC_READ | GENERIC_WRITE,
+                             FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+                             OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
+  SetStdHandle(STD_ERROR_HANDLE, hConOut);
+  SetStdHandle(STD_INPUT_HANDLE, hConIn);
+  std::wcout.clear();
+  std::wcerr.clear();
+  std::wcin.clear();
+}
+
+void PrintHelp() {
+  std::wcout << std::endl;
+  std::wcout << "Usage:" << std::endl;
+  std::wcout << "    -l --licenses - Shows open source licenses" << std::endl;
+  std::wcout << "    -h --help     - Shows this usage info" << std::endl;
+  std::wcout << std::endl;
+  std::ios::sync_with_stdio();
+}
+
+void OpenLicenses() {
+  std::filesystem::path rootPath =
+      std::filesystem::temp_directory_path() / L"D3d12infoGUI";
+  if (!std::filesystem::is_directory(rootPath)) {
+    if (!std::filesystem::create_directory(rootPath)) {
+      throw std::runtime_error("Failed to create temp directory");
+    }
+  }
+
+  EmbedExctactor::ExtractFiles(rootPath);
+
+  auto D3d12infoGUILicense = rootPath / L"D3d12infoGUI_LICENSE.txt";
+  ::ShellExecuteW(NULL, L"open", D3d12infoGUILicense.c_str(), NULL, NULL,
+                  SW_SHOW);
+#if !USE_D3D12INFO_FROM_WORKING_DIRECTORY
+  auto D3d12infoLicense = rootPath / L"D3d12info_LICENSE.txt";
+  ::ShellExecuteW(NULL, L"open", D3d12infoLicense.c_str(), NULL, NULL,
+                  SW_SHOW);
+#endif
+}
+
+int ProcessCommandLine(LPWSTR lpCmdLine) {
+  if (std::wcscmp(lpCmdLine, L"--licenses") == 0 ||
+      std::wcscmp(lpCmdLine, L"-l") == 0) {
+    OpenLicenses();
+    return 0;
+  }
+
+  if (std::wcscmp(lpCmdLine, L"--help") == 0 ||
+      std::wcscmp(lpCmdLine, L"-h") == 0) {
+    PrintHelp();
+    return 0;
+  }
+
+  PrintHelp();
+  return 1;
+}
+
+int RunCommandLine(LPWSTR lpCmdLine) {
+  InitializeConsole();
+
+  int result = ProcessCommandLine(lpCmdLine);
+
+  ::FreeConsole();
+
+  return result;
+}
+
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                       LPWSTR lpCmdLine, int nCmdShow) {
   try {
-    return mainImpl(hInstance);
+    if (*lpCmdLine != L'\0') {
+      return RunCommandLine(lpCmdLine);
+    }
+    return RunGUI(hInstance);
   } catch (const std::exception& e) {
     MessageBoxA(NULL, e.what(), "Error", MB_OK | MB_ICONERROR);
     return 1;
