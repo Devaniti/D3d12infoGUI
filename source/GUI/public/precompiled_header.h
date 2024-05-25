7955849c1f549186ef4898d46dd4ca0bb0b1a117
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2024-05-25 20:26:58 +0100

    Fixed missing includes

--- source/GUI/public/precompiled_header.h
+++ source/GUI/public/precompiled_header.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <cassert>
+#include <condition_variable>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
 #include <thread>
