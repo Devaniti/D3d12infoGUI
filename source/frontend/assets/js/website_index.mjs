b89740f6c9d2bc19c915e55c03726db1ae7df8c8
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2025-04-09 01:48:53 +0900

    Updated title logic on main page. Updated title in feature table.

--- source/frontend/assets/js/website_index.mjs
+++ source/frontend/assets/js/website_index.mjs
@@ -6,7 +6,16 @@ function OnLoad() {
 
     Server.GetAllSubmissions((reports) => {
         let queryParams = (new URL(document.location)).searchParams
-        ReportViewer.LoadReports(reports, queryParams.get('ID'))
+        let reportID = queryParams.get('ID')
+        ReportViewer.LoadReports(reports, reportID)
+
+
+        if (reportID !== null) {
+            var report = reports.find(r => r.GetField("ID") == reportID)
+            if (report != null) {    
+                document.title = report.GetField("DXGI_ADAPTER_DESC3.Description") + " - D3d12infoDB";
+            }
+        }
     })
 }
 
